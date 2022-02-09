d57ebe6d0ed7ae44d54ac4f432be1f17e4acceda
Brigitte <bs@hangar49.com>
2022-02-09 23:08:21 +0200

    More urls and views for upload and client pages

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name='index'),
+    path('upload/<int:request_id>', views.UploadFileView.as_view(), name='upload'),
+    path('client/<int:client_id>', views.ClientView.as_view(), name='client')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from dashboard.models import *
 
 
 def index(request):
@@ -10,4 +11,40 @@ class DashboardView(generic.DetailView):
     template_name = "dashboard/index.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        # Relationship manager is hardset as the first one in the list
+        clients = Client.objects.filter(relationshipManager_id=1)
+        context = {
+            "clients": clients
+        }
+        return render(request, self.template_name, context)
+
+class ClientView(generic.DetailView):
+    template_name = "dashboard/client_page.html"
+
+    def get(self, request, *args, **kwargs):
+        id = kwargs['client_id']
+        client = Client.objects.get(id=id)
+        docRequests = DocumentRequest.objects.filter(client=client)
+        context = {
+            "client": client,
+            "requests": docRequests
+        }
+        return render(request, self.template_name, context)
+
+
+class UploadFileView(generic.DetailView):
+    template_name = "documents/upload_page.html"
+
+    def get(self, request, *args, **kwargs):
+        # Get request ID from kwargs
+        id = kwargs['request_id']
+        docRequest = DocumentRequest.objects.get(id=id)
+        return render(request, self.template_name, {'request': docRequest})
+
+    def post(self, request, *args, **kwargs):
+        id = kwargs['request_id']
+        docRequest = DocumentRequest.objects.get(id=id)
+        docName = docRequest.client.name + " - " + docRequest.type
+        document = Document(name=docName, file=request.FILES['filename'], documentRequest=docRequest)
+        document.save()
+        return HttpResponse("Successful")
