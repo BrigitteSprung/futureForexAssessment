a23ab4bdc4da71fb6e1e99ef1339137ff74cd2b7
Brigitte <bs@hangar49.com>
2022-02-09 10:27:50 +0200

    Registered models to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+# ENGAGEMENT HANDOVER
+class RelationshipManagerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    # list_filter = ['client']
+    # raw_id_fields = ('client',)
+
+# ENGAGEMENT HANDOVER
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'relationshipManager')
+    # list_filter = ['client']
+    raw_id_fields = ('relationshipManager',)
+
+# ENGAGEMENT HANDOVER
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ['client']
+    raw_id_fields = ('client','relationshipManager')
+
+
+
+
+
+
+admin.site.register(RelationshipManager, RelationshipManagerAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Document, DocumentAdmin)
