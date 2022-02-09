2e6a6ee35836fa2c0ac3494d8024c614423e3206
Brigitte <bs@hangar49.com>
2022-02-09 10:28:18 +0200

    Added view and url for test dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.views import generic
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+class DashboardView(generic.DetailView):
+    template_name = "dashboard/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
