fcf20eb50634f4ea3336c91455978fbbbed7be29
Brigitte <bs@hangar49.com>
2022-02-14 16:26:57 +0200

    Fixed order of admin and media urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.conf.urls.static import static
+
+from futureForexAssessment import settings
 
 from . import views
 
 urlpatterns = [
-    path('', views.DashboardView.as_view(), name='index'),
+    path('', views.DashboardView.as_view(), name='dashboard'),
     path('upload/<int:request_id>', views.UploadFileView.as_view(), name='upload'),
-    path('client/<int:client_id>', views.ClientView.as_view(), name='client')
+    path('client/<int:client_id>', views.ClientView.as_view(), name='client'),
+    path('clients/', views.ClientsView.as_view(), name='clients'),
+    path('request/<int:client_id>', views.RequestView.as_view(), name='request'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- futureForexAssessment/urls.py
+++ futureForexAssessment/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('dashboard.urls'), name='home'),
     path('admin/', admin.site.urls),
+    path('', include('dashboard.urls'), name='home'),
+
 ]
